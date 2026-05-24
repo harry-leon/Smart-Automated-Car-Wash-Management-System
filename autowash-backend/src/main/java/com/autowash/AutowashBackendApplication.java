ef0471dc169725d4821f59f93dd0ee1beaaa0783
Ha Thuc Quoc Hung <htquochung.dev@gmail.com>
2026-05-25 02:44:24 +0700

    fix: restore backend spring boot entrypoint

---
+++ autowash-backend/src/main/java/com/autowash/AutowashBackendApplication.java
@@ -0,0 +1,12 @@
+package com.autowash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutowashBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutowashBackendApplication.class, args);
+    }
+}
